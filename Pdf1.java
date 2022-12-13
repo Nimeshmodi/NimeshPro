2e61e448659ee1d3d4e1da0fcfb61d51cbd54a97
Nimeshmodi <modinimesh81@gmail.com>
2022-12-13 20:03:31 +0000

    first commit

---
+++ Pdf1.java
@@ -0,0 +1,16 @@
+package Manual_softtware_testing1;
+
+public class Pdf1 {
+    public static void main(String[] args) {
+        System.out.println("Qualification Of Tester");
+        System.out.println("types of software");
+        System.out.println("WHAT IS A SOFTWARE?");
+        System.out.println("WHAT IS SOFTWARE TESTING?");
+        System.out.println("WHAT ARE THE BENEFITS OF SOFTWARE TESTING?");
+        System.out.println("WHO DOES TESTING?");
+        System.out.println("Challenges Of Tester");
+        System.out.println("WHAT IS MANUAL TESTING?");
+        System.out.println("SDLC Software Development Life Cycle");
+        System.out.println("STLC Software Testing Life Cycle");
+    }
+}
---
+++ Pdf2.java
@@ -0,0 +1,18 @@
+package Manual_softtware_testing1;
+
+public class Pdf2 {
+    public static void main(String[] args) {
+        System.out.println("Challenges with manual testing");
+        System.out.println("7 PRINCIPLES OF SOFTWARE TESTING");
+        System.out.println("Test plan template");
+        System.out.println("Test scenario");
+        System.out.println("Why we do write test scenario");
+        System.out.println("Test case");
+        System.out.println("Why we do write test case");
+        System.out.println("test scenario template");
+        System.out.println("test case template");
+        System.out.println("Requirement Traceability Matrix");
+        System.out.println("test scenario vs test case");
+        System.out.println("SOFTWARE TESTING TECHNIQUES");
+    }
+}
